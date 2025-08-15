bd7ee1231b43dd47dd3824efa339e5c2b044b700
RITUL DE <ritul-de-bl19-2008@gcect.ac.in>
2025-08-15 22:32:18 +0530

    User should not access any route if not login

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navbar } from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Footer } from "./Footer";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios";
 
 export const Body = () => {
+  const dispatch = useDispatch();
+  const navigate=useNavigate();
+  const userData=useSelector((store)=>store.user);
+    const fetchUser = async (req,res) => {
+      if(userData) return;
+      try{
+        const user= await axios.get(BASE_URL+"/profile/view",{
+          withCredentials: true
+        });
+        dispatch(addUser(res.data))
+      }catch(err){
+        if(err.status===401){
+          navigate("/login");
+        }
+        console.log(err);
+      }
+    };
+    useEffect(()=>{
+        fetchUser(); 
+      },[]);
   return (
     <>
       <Navbar />
       <Outlet />
-      <Footer/>
+      <Footer />
     </>
   );
 };
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
 
@@ -9,7 +10,7 @@ export const Navbar = () => {
   return (
           <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
         {user && <div className="flex gap-2">
           <p className='px-4'>Welcome {user.firstName}</p>
@@ -31,10 +32,10 @@ export const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
